1ecc0a73e4b31e5e22a983560257f7678fe7d2c4
timurozkul <tim88r@gmail.com>
2016-11-11 11:22:12 +0000

    data structure changes & mockup picture

--- AppreciationApp/Appreciation/server/methods.js
+++ AppreciationApp/Appreciation/server/methods.js
@@ -1,5 +1,15 @@
 Meteor.methods({
-    'addUser'(profile) {
+    // 'addUser'(profile) {
+    //     Users.insert(profile);
+    // },
+    'addSubject'(userId, subject) {
         Users.insert(profile);
     }
+    
 });
+//       Meteor.users.update({_id: Meteor.userId()}, {$addToSet: {coursesTaught: courseTaught}});
+
+// editAssessment(oldAssessmentId, newAssessment){
+//     Assessment.update({_id: oldAssessmentId}, {$set:
+//     {date: newAssessment.date, time: newAssessment.time, location: newAssessment.location, postcode: newAssessment.postcode } });
+// }
