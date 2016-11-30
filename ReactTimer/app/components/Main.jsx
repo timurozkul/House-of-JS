58aa46ded5d8ac5c72f92c227e1c3f2ef8f4f39c
timurozkul <tim88r@gmail.com>
2016-11-30 15:51:18 +0000

    Countdown input

--- ReactTimer/app/components/Main.jsx
+++ ReactTimer/app/components/Main.jsx
@@ -4,10 +4,9 @@ var Nav = require('Nav');
 var Main = (props) => {
     return (
         <div>
-            <div>
-                <div>
-                    <Nav/>
-                    <p>Main.jsx Rendered</p>
+            <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
                     {props.children}
                 </div>
             </div>
