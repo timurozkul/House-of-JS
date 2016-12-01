5e592fe15f95dbdad47907bee38d2f7ff4bf74b0
timurozkul <tim88r@gmail.com>
2016-12-01 15:37:05 +0000

    start, pause, clear buttons functional

--- ReactTimer/app/components/Countdown.jsx
+++ ReactTimer/app/components/Countdown.jsx
@@ -1,25 +1,62 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 
 var Countdown = React.createClass({
     getInitialState: function () {
-        return {count: 0};
+        return {
+            count: 0,
+            countdownStatus: 'stopped'
+        };
     },
-
+    componentDidUpdate: function (prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+                case 'stopped':
+                    this.setState({count: 0});
+                case 'paused':
+                    clearInterval(this.timer)
+                    this.timer = undefined;
+                    break;
+            }
+        }
+    },
+    startTimer: function () {
+        this.timer = setInterval(() => {
+            var newCount = this.state.count - 1;
+            this.setState({
+                count: newCount >= 0 ? newCount : 0
+            });
+        }, 1000);
+    },
     handleSetCountdown: function (seconds) {
         this.setState({
-            count: seconds
-        })
+            count: seconds,
+            countdownStatus: 'started'
+        });
     },
-
+    handleStatusChange: function (newStatus) {
+        this.setState({countdownStatus: newStatus});
+    },
     render: function () {
-        var {count} = this.state;
+        var {count, countdownStatus} = this.state;
+        var renderControlArea = () => {
+            if (countdownStatus !== 'stopped') {
+                return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>;
+            } else {
+                return <CountdownForm onSetCountdown={this.handleSetCountdown}/>;
+            }
+        };
+
         return (
             <div>
                 <Clock totalSeconds={count}/>
-                <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+                {renderControlArea()}
             </div>
         );
     }
--- ReactTimer/webpack.config.js
+++ ReactTimer/webpack.config.js
@@ -28,7 +28,8 @@ module.exports = {
       Timer: 'app/components/Timer.jsx',
       Countdown: 'app/components/Countdown.jsx',
       CountdownForm: 'app/components/CountdownForm.jsx',
-      Clock: 'app/components/Clock.jsx'
+      Clock: 'app/components/Clock.jsx',
+      Controls: 'app/components/Controls.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
