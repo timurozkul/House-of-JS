ca2102a56654bac7cfb1e5f778401ad0be166d96
timurozkul <tim88r@gmail.com>
2016-12-07 16:24:18 +0000

    init redux implementation

---
+++ ReactTodo/app/reducers/reducers.jsx
@@ -0,0 +1,38 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+export var searchTextReducer = (state = '', action) => {
+    switch (action.type) {
+        case "SET_SEARCH_TEXT":
+            return action.searchText;
+        default:
+            return state;
+    }
+};
+
+export var showCompletedReducer  = (state = false, action) => {
+    switch (action.type) {
+        case "TOGGLE_SHOW_COMPLETED":
+            return !state;
+        default:
+            return state;
+    }
+};
+
+export var todosReducer  = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [
+                ...state,
+                {
+                    id: uuid(),
+                    text: action.text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ];
+        default:
+            return state;
+    }
+};
