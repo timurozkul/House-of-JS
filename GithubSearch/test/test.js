e7af1d4bc289ceaad38d9456aa2c6db3e34ebf23
timurozkul <tim88r@gmail.com>
2016-12-18 16:52:27 +0000

    add new project

---
+++ GithubSearch/test/test.js
@@ -0,0 +1,30 @@
+// var should = require("should");
+// var request = require("request");
+// var expect = require("chai").expect;
+// var baseUrl = "https://api.github.com/users/timurozkul";
+// var util = require("util");
+
+
+// describe('API', function() {
+//     describe('return user', function() {
+//         it('should return 200 status & username', function(done) {
+//             // .set('User-Agent', "some spoofed agent")
+//             //
+//             request.get({ url: baseUrl },
+//                function(error, response, body) {
+//                // var bodyObj = JSON.parse(body);
+//                // expect(bodyObj.username).to.equal('timurozkul');
+// console.log(error)
+//                expect(response.statusCode).to.equal(200);
+//                console.log(body);
+//                done();
+//
+//
+//
+//
+//
+//            }
+//            )
+//         });
+//     });
+// });
