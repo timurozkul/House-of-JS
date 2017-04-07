08ea1a7c270150d5879c2fc385860c0c5665cb99
timurozkul <tim88r@gmail.com>
2017-04-07 13:14:16 +0100

    restructure

--- NotesNode/app.js
+++ NotesNode/app.js
@@ -10,17 +10,26 @@ const notes = require('./notes.js');
 const argv = yargs.argv;
 var command = argv._[0];
 
-// console.log('Command:', command);
 console.log('Yargs:', argv);
 
 if(command === 'add'){
-	notes.addNote(argv.title, argv.body);
+	var note = notes.addNote(argv.title, argv.body);
+	if(note) {
+		console.log('Note created');
+		console.log('---');
+		console.log('Title: ' + note.title);
+		console.log('Body: ' + note.body);
+	} else {
+		console.log('Note title taken');
+	}
 } else if (command === 'list'){
 	notes.getAll();
 } else if (command === 'read'){
 	notes.getNote(argv.title);
 } else if (command === 'remove'){
-	notes.removeNote(argv.title);
+	var noteRemoved = notes.removeNote(argv.title);
+	var message = noteRemoved ? 'Note was removed' : 'Note not found';
+	console.log(message);
 } else {
 	console.log('command not recognized');
 }
