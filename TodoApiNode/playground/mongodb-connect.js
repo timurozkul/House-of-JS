2507178e29128dd6aa2d159b81054ebbfce29610
timurozkul <tim88r@gmail.com>
2017-05-05 15:11:31 +0100

    Init todo app

---
+++ TodoApiNode/playground/mongodb-connect.js
@@ -0,0 +1,24 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+	if(err){
+		return console.log('unable to connect to MongoDB server');
+	}
+
+	console.log('Connected to MongoDB server');
+	db.collection('Todos').insertOne({
+		text: 'Yello',
+		completed: false
+	}, (err, result) => {
+
+		if(err){
+			return console.log('Unable to insert todo', err);
+		}
+
+		console.log(JSON.stringify(result.ops, undefined, 2 ));
+	});
+
+	db.close();
+});
