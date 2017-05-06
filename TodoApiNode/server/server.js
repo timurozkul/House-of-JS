441bf4b5967bea6f8f46ceecb8423fae106c17ad
timurozkul <tim88r@gmail.com>
2017-05-06 16:33:10 +0100

    TodoApiNode: testing get todos

--- TodoApiNode/server/server.js
+++ TodoApiNode/server/server.js
@@ -20,15 +20,11 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-  // console.log('Testttt')
 });
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({
-      todos,
-
-    });
+    res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   })
